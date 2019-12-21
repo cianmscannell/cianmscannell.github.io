dfd3615c6b02d72f8df70741420a9139d6bd3b69
cianmscannell <cianmscannell@gmail.com>
2019-12-21 16:19:22 +0000

    pubs

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -93,12 +93,12 @@ for row, item in publications.iterrows():
     ## Markdown description for individual page
     
     if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
+        md += item.citation + "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
     # if len(str(item.excerpt)) > 5:
     #     md += "\n" + html_escape(item.excerpt) + "\n"
         
-    md += "\n" + item.citation
+    # md += "\n" + 
     
     md_filename = os.path.basename(md_filename)
        
